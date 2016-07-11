60c05b48f1ad7333eef46ce78826c9b8630d37c3
Cm_Star <cm_star>
2016-07-11 01:40:30 -0400

    Fix location of manifests in vanila init script

--- vanilla/init.sh
+++ vanilla/init.sh
@@ -5,11 +5,11 @@ printf "Downloading version manifest...\n"
   cd /tmp/
   wget -q "https://launchermeta.mojang.com/mc/game/version_manifest.json"
   wget -qO manifest.json \
-    $(printf $(jq -r --arg ver "$VERSION" --arg rel "$(jq -r '.latest.release' < /version_manifest.json)" \
-    '(.versions[] | select(.id == $ver).url),(.versions[] | select(.id == $rel).url)' < /version_manifest.json) \
+    $(printf $(jq -r --arg ver "$VERSION" --arg rel "$(jq -r '.latest.release' < version_manifest.json)" \
+    '(.versions[] | select(.id == $ver).url),(.versions[] | select(.id == $rel).url)' < version_manifest.json) \
     | awk '{print $1}')
-  printf "Downloading Minecraft server ($(jq -r '.id' < /manifest.json))...\n"
-  wget -qO /minecraft/server.jar "$(jq -r '.downloads.server.url' < /manifest.json)"
+  printf "Downloading Minecraft server ($(jq -r '.id' < manifest.json))...\n"
+  wget -qO /minecraft/server.jar "$(jq -r '.downloads.server.url' < manifest.json)"
   printf "Removing manifests...\n"
   rm -rf *
 )
