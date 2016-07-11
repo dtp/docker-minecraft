2023cdf301d85e10eb69be21954f6f752acd0f84
Cm_Star <cm_star>
2016-07-11 01:17:42 -0400

    Use VERSION variable for vanilla init script

--- vanilla/init.sh
+++ vanilla/init.sh
@@ -5,7 +5,7 @@ printf "Downloading version manifest...\n"
   cd /tmp/
   wget -q "https://launchermeta.mojang.com/mc/game/version_manifest.json"
   wget -qO manifest.json \
-    $(printf $(jq -r --arg ver "$2" --arg rel "$(jq -r '.latest.release' < /version_manifest.json)" \
+    $(printf $(jq -r --arg ver "$VERSION" --arg rel "$(jq -r '.latest.release' < /version_manifest.json)" \
     '(.versions[] | select(.id == $ver).url),(.versions[] | select(.id == $rel).url)' < /version_manifest.json) \
     | awk '{print $1}')
   printf "Downloading Minecraft server ($(jq -r '.id' < /manifest.json))...\n"
