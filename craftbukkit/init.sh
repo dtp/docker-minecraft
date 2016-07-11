0b4e75237bfa13d82ec10a2cda75facbb2500e98
Cm_Star <cm_star>
2016-07-11 01:48:03 -0400

    Use JVM_OPTS for BuildTools

--- craftbukkit/init.sh
+++ craftbukkit/init.sh
@@ -5,7 +5,7 @@
   printf "Downloading BuildTools...\n"
   wget -q "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
   printf "Building CraftBukkit...\n"
-  bash -c "java -jar BuildTools.jar --rev $VERSION"
+  bash -c "java $JVM_OPTS -jar BuildTools.jar --rev $VERSION"
   mv craftbukkit*.jar /craftbukkit/craftbukkit.jar
   printf "Clean up...\n"
   rm -rf *
